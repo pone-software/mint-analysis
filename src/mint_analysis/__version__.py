fb945fbf4f00ef46fbc2c6d015ea6b90bbdf2a84
George Marshall <george.marshall.20@ucl.ac.uk>
2025-01-29 15:36:54 +0000

    more pc fixes

--- src/mint_analysis/__version__.py
+++ src/mint_analysis/__version__.py
@@ -1,4 +1,5 @@
 """
 Mint-analysis: processing PMT data from P-ONE
 """
+
 __version__ = "0.1.0"
--- src/mint_analysis/dsp/__init__.py
+++ src/mint_analysis/dsp/__init__.py
@@ -6,4 +6,4 @@ from .build_nnls_matrix import build_nnls_matrix
 from .lib_pulse_reco import pulse_analysis
 from .template_wf import build_template_waveform
 
-__all__ = ["build_nnls_matrix", "pulse_analysis", "build_template_waveform"]
+__all__ = ["build_nnls_matrix", "build_template_waveform", "pulse_analysis"]
