a7035d8f80eb77737935a8d39f2969a1c5884932
vgousyleblanc <v.gousy-leblanc@tum.de>
2025-06-13 15:46:04 -0700

    modified:   interface/mist_interface.py

--- mint_devices/interface/mist_interface.py
+++ mint_devices/interface/mist_interface.py
@@ -1,1 +1,6 @@
 #Code describing the interface code between the mist and the mainboard
+
+
+
+
+#Need to Record coincidences for half-hour interval  with each MIST using nominal settings. and verify expected rate and amplitude distribution push to the database
