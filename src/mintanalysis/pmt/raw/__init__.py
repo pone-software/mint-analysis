b4290274ae1c1cdaef6157b175e9ef064c7922a7
Patrick Krause <pkrause@sfu.ca>
2025-12-16 19:31:20 -0800

    Moved deprecated code to attic

--- src/mintanalysis/pmt/raw/__init__.py
+++ src/mintanalysis/pmt/raw/__init__.py
@@ -3,6 +3,5 @@ Routines for reading eng files and converting to lh5
 """
 
 from .build_raw import build_raw, main
-from .eng_reader import EngFormatReader
 
-__all__ = ["EngFormatReader", "build_raw", "main"]
+__all__ = ["build_raw", "main"]
